ec48cc15479a8a7706d83e90fa78f0435ba2121a
loitp <www.muathu@gmail.com>
2023-05-02 20:18:10 +0700

    up

--- src/androidMain/kotlin/ds/tetris/android/App.kt
+++ src/androidMain/kotlin/ds/tetris/android/App.kt
@@ -2,11 +2,17 @@ package ds.tetris.android
 
 import android.app.Application
 import ds.tetris.di.gameModule
-import ds.tetris.di.initKoin
 import ds.tetris.di.platformModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
+//TODO ic_launcher
+//TODO ad
+//TODO firebase
+//TODO rate app
+//TODO share app
+//TODO more app
+//TODO policy
 class App : Application() {
 
     override fun onCreate() {
